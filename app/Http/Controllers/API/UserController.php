7faf12ea80426a30c5646b4658ef70e39a0ef829
Vigikaran Vijayaratnam <vigi@hi2mail.com>
2020-11-26 18:37:17 +0000

    mehod addded

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -24,6 +24,7 @@ class UserController extends Controller
                     'all'  =>  $request->all(),
                     'content' => $request->getContent(),
                     'parsed' => GlobalSim::webhookContent($request->getContent()),
+                    'method' => $request->method(),
                     'query' => $request->query()
                 ]);
 
