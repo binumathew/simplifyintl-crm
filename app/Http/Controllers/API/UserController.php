d6878db587c252f3ed3885889a8876a85db6de9f
Vigikaran Vijayaratnam <vigi@hi2mail.com>
2020-11-27 08:59:01 +0000

    bug fixed

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Jobs\FetchCDR\GlobalSim;
+use App\Helpers\GlobalSim;
 use Log;
 
 class UserController extends Controller
