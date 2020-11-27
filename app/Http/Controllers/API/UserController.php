1275f134fadc2626bdb1bb62ecd66b4b2e61f36a
Vigikaran Vijayaratnam <vigi@hi2mail.com>
2020-11-27 09:56:55 +0000

    bug fixed

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -44,10 +44,9 @@ class UserController extends Controller
 	{
             try{
                 $qrcode = gzencode(Utils::qrcode('LPA:1$ecprsp.eastcompeace.com$2D5DB6B3962D4ECC8C871E068708900D') ,9);
-                return response($qrcode)
-                        ->header('Content-Type','image/svg+xml')
-                        ->header('Vary', 'Accept-Encoding')
-                        ->header('Content-Encoding', 'gzip');
+                return [
+                    'code'  => $qrcode
+                ];
             }catch(\Exception $e){
                 return [
                     'result'    => false,
