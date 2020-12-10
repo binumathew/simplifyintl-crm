fdfa5d3a9c3d447d444cb0447ca6b71a44a1b2d2
ArunRaj <arun@gencomtel.com>
2020-12-11 01:06:16 +0530

    bug fixed

--- app/Jobs/Delivery/SoftDelivery.php
+++ app/Jobs/Delivery/SoftDelivery.php
@@ -54,7 +54,7 @@ class SoftDelivery //implements ShouldQueue
                     $obj->date      = Carbon::now()->format('d M Y');
     
                     Mail::to($user->email)
-                        //->bcc(['jijo.joseph@gencomtel.com','support@avoomobile.com'])
+                        ->bcc(['jijo.joseph@gencomtel.com','arun@gencomtel.com'])
                         ->send(new SimSoftDelivery($obj));                   
                 }
             }
