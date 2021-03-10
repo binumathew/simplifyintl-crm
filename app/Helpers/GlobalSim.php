401486eb70d6834dcebef45076245d943e67cd32
ArunRaj <arun@gencomtel.com>
2021-03-10 17:47:03 +0530

    modify user

--- app/Helpers/GlobalSim.php
+++ app/Helpers/GlobalSim.php
@@ -243,6 +243,7 @@ class GlobalSim {
             Log::info('modifycustomerrequest',[
                 'request'=>$params
             ]);
+
             $result = self::get($params,'ModifyCustomer');
             if($result !== false) {
                 return  json_decode(json_encode(simplexml_load_string($result)),true);
@@ -311,13 +312,13 @@ class GlobalSim {
                     'Username'=>$customer_id,
                     'Password'=>Helper::random(8),
                     'Title'=>'',
-                    'FirstName'=>$user->first_name,
+                    'FirstName'=>isset($user->first_name) ? $user->first_name : $user->parent->first_name,
                     'MiddleInitials'=>'',
-                    'Surname'=>$user->last_name,
+                    'Surname'=>isset($user->last_name) ? $user->first_name : $user->parent->last_name,
                     'Country'=>$user->country->short_code,
                 ],
                 'Contact' =>[
-                    'Email'=>$user->email,
+                    'Email'=>isset($user->email) ? $user->email : $user->parent->email,
                     'CallKeyID'=>$customer_id,
                 ],
                 'Address' =>[
