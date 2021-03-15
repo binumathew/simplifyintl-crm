03437ca598c01b41094bd16c3640aaa43cf0af0e
ArunRaj <arun@gencomtel.com>
2021-03-15 23:03:10 +0530

    inv feb

--- app/Console/Commands/InvoiceGeneration.php
+++ app/Console/Commands/InvoiceGeneration.php
@@ -49,7 +49,7 @@ class InvoiceGeneration extends Command
         if(ScheduledTask::where(['command' => $this->signature, 'status' => 1])->exists()){
             try {
                 $start_time      = microtime(true);
-                $currDay         = '2021-01-01';//Carbon::now()->startOfMonth()->toDateString();
+                $currDay         = '2021-02-01';//Carbon::now()->startOfMonth()->toDateString();
                 $getsubscription =  AutoPlan::select('auto_plan.id')
                                     ->where(function($query) use ($currDay){
                                         $query->where('auto_plan.status',1);
