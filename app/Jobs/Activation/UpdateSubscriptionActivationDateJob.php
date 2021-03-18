64a123af879c1782acf175ae4b29064ec6e6e798
ArunRaj <arun@gencomtel.com>
2021-03-18 17:08:48 +0530

    subscription date

--- app/Jobs/Activation/UpdateSubscriptionActivationDateJob.php
+++ app/Jobs/Activation/UpdateSubscriptionActivationDateJob.php
@@ -45,13 +45,13 @@ class UpdateSubscriptionActivationDateJob implements ShouldQueue
                     $result = DwpHelper::dwp_response($response->children);
                     if(is_array($result) && is_array($result['orders']) && isset($result['orders']['block']['components']['block']['completion-date']) && $result['orders']['block']['components']['block']['completion-date'] != ""){
                         $comp_date = $result['orders']['block']['components']['block']['completion-date'];
-                        $result = AutoPlan::where('id',$this->plan_id)
+                        $result = AutoPlan::where('id',$this->autoplan_id)
                                     ->limit(1)
                                     ->update(['start_date'=>$comp_date,'act_check'=>1]);
                     }else{
                         Log::info('DwpHelperError',[
                             'result' => $result,
-                            'autoplan_id'=>$this->plan_id
+                            'autoplan_id'=>$this->autoplan_id
                         ]);  
                     }
                 }
