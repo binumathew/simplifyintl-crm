d976dc28226ec0d6e0c67baa08324473ca62071a
ArunRaj <arun@gencomtel.com>
2021-03-22 18:39:24 +0530

    inv download

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -146,8 +146,8 @@ class InvoiceController extends Controller
             //$view = view('invoice.user_invoice', compact('invoiceData','user'))->render();
             $filename = explode('-',$invoice->date)[1].explode('-',$invoice->date)[0].'_'.$invoice->id.'_'.$user->userDetail->user_platform.$user->id.'.pdf';
             $pdf = MPDF::loadView('invoice.user_invoice', compact('invoiceData','user'));
-            //return $pdf->download($filename);
-            return $pdf->stream($filename);
+            return $pdf->download($filename);
+            //return $pdf->stream($filename);
         }else{
             dd('Invoice Details not found.');
         }
