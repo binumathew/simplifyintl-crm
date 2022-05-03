8f107e08d59deba6ec33ec8b56d38776acc468f3
ArunRaj <arun@gencomtel.com>
2022-05-03 16:53:47 +0530

    monthly cdr cron

--- app/Console/Commands/CallsMonthlySummary.php
+++ app/Console/Commands/CallsMonthlySummary.php
@@ -6,12 +6,12 @@ use DB,Mail,Helper,Carbon,Storage;
 use Illuminate\Console\Command;
 use Cron\CronExpression;
 use Log;
-
 use App\Mail\CronFailure;
 use App\Models\ScheduledTask;
 use Illuminate\Console\Scheduling\Schedule;
 
 use App\Jobs\CDR\MonthlyCdrUpdateJob;
+use App\Jobs\CDR\SimUsageSummaryJob;
 class CallsMonthlySummary extends Command
 {
     /**
@@ -54,27 +54,23 @@ class CallsMonthlySummary extends Command
             $now = $currday->format('Mpy');
             $ftpusername = config('services.dwp.simftp_username');
             $remotefile = '/Monthly/'.$year.'/Mobile_'.strtoupper($now).'_'.$ftpusername.'_Calls.CSV';
-
-            $localfile  = strtoupper($now).'.csv';
-            $exists = Storage::disk('calllogs')->exists($localfile);
-
-            if($exists){
-                MonthlyCdrUpdateJob::dispatch($localfile);
-            }else{
-                $getfile = $this->getfileFTP($remotefile,$localfile);
-                if($getfile){
-                    MonthlyCdrUpdateJob::dispatch($localfile);
-                }
-            }
+            $getfile = $this->getfileFTP($remotefile);
         }
     }
     //Ftp connection
-    private function getfileFTP($remotefile,$localfile){
-        $ftp    = Storage::disk('ftp');
+    private function getfileFTP($remotefile){
+        $ftp    = Storage::disk('sftp');
         try {
-            $file = $ftp->get($remotefile);
-            Storage::disk('calllogs')->put($localfile, $file);
-            $ftp->getDriver()->getAdapter()->disconnect();
+            $getFile       = $ftp->readStream($remotefile);
+            //$getFile = Storage::disk('gcs')->readStream($remotefile);
+            Log::info('calls-monthly-summary-get-file',[
+                'params'=>$remotefile
+            ]);
+            self::processUsage($getFile);
+            //$ftp->getDriver()->getAdapter()->disconnect();
+            // Log::info('calls-monthly-summary',[
+            //     'params'=>$remotefile
+            // ]);
         } catch (\Exception $e) {
             $task = ScheduledTask::where(['command'=>$this->signature,'status'=>1])->first();
             $obj = (object)['subject' => 'Cron Failure '.config('settings.app_name').Carbon::now()->format('Y-m-d'), 'heading' => 'Cron Failure '.config('settings.app_name'), 'cron' => $task->description, 'error' => $e->getMessage()];
@@ -84,5 +80,41 @@ class CallsMonthlySummary extends Command
             return false;
         }
         return true;
+    }
+    private function processUsage($file){
+
+        //Remove existing old CDR from DB
+        $start_month = Carbon::now()->subMonth(1)->format('Y-m-01');
+        $end_month   = Carbon::parse($start_month)->endOfMonth()->format('Y-m-d');
+
+        DB::table('usage_history')->whereIn('provider', ['O2','VF'])
+                                  ->whereDate('date', '>=',$start_month)
+                                  ->whereDate('date', '<=',$end_month)
+                                  ->delete();
+
+        DB::table('user_calls')->whereIn('provider', ['O2','VF'])
+                                    ->whereDate('connect_date', '>=',$start_month)
+                                    ->whereDate('connect_date', '<=',$end_month)
+                                    ->delete();
+        $cdrData    = [];
+        $skipheader = true;
+        while ($csvLine = fgetcsv($file, 1000, ",")) {
+            if($skipheader){ $skipheader = false; continue;}
+            else{
+                $cdrData[] = $csvLine;
+                if(count($cdrData) == 1000){
+                    MonthlyCdrUpdateJob::dispatch($cdrData);
+                    $cdrData = [];
+                }
+            }
+        }
+        MonthlyCdrUpdateJob::dispatch($cdrData); 
+
+        Log::info('calls-monthly-summary-process-complete',[
+                'start_month'=>$start_month,
+                'end_month'=>$end_month
+            ]);
+
+        SimUsageSummaryJob::dispatch($start_month, $end_month);
     }
 }
