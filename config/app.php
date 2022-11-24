fad95c63b4461cafdd0c05c0e3807666d8e3d1c9
ArunRaj <arun@gencomtel.com>
2022-11-24 14:47:50 +0530

    horizon

--- config/app.php
+++ config/app.php
@@ -368,6 +368,7 @@ return [
         Maatwebsite\Excel\ExcelServiceProvider::class,
         Meneses\LaravelMpdf\LaravelMpdfServiceProvider::class,
         Superbalist\LaravelGoogleCloudStorage\GoogleCloudStorageServiceProvider::class,
+        App\Providers\HorizonServiceProvider::class,
 
     ],
 
